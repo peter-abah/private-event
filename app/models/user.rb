d2ae7bc95528ccc53c9c491026bb4590e53df455
Peter Abah <peterabah.ob@gmail.com>
2021-07-22 18:45:29 +0100

    add markup in views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events
+
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
